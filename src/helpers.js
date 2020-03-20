93d53cafad0df8a724ee62b31b84f5c19cf27080
Rehemae Anton <anton.rehemae@enlabs.se>
2020-03-21 00:52:02 +0200

    add CI and bages

---
+++ src/helpers.js
@@ -0,0 +1,10 @@
+import url from 'url';
+
+// eslint-disable-next-line import/prefer-default-export
+export const generateFileName = (link) => {
+  const { auth, host, path } = url.parse(link);
+  const partsOfName = [auth, host, path];
+  return `${partsOfName.filter((partOfName) => partOfName !== null && partOfName).join('-').replace(/-\/$/, '')
+    .replace(/[\W]/g, '-')
+    .replace(/-{1,}/g, '-')}.html`.replace(/-html\.html$/, '.html');
+};
