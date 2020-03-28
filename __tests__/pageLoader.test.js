8ca2f21dfbfa9c86c8e7b60e941080f405508586
Rehemae Anton <anton.rehemae@enlabs.se>
2020-03-28 23:08:13 +0200

    fix tempdir tests

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -1,15 +1,15 @@
 import nock from 'nock';
 import { promises as fs } from 'fs';
 import os from 'os';
-import _ from 'lodash';
 import path from 'path';
 import pageLoader from '../src';
 
-const tempDir = path.join(os.tmpdir(), 'pageLoader');
+const genNameTempDir = () => path.join(os.tmpdir(), 'page-loader-');
 const getPathToFixture = (fileName) => path.join(__dirname, '__fixtures__', fileName);
+let tempDir;
 
 beforeEach(async () => {
-  await fs.unlink(tempDir).catch(_.noop);
+  tempDir = await fs.mkdtemp(genNameTempDir());
 });
 
 beforeAll(async () => {
@@ -35,12 +35,12 @@ describe('get http request to page and save html', () => {
   test('page should not be saved, if request fails', async () => {
     await expect(pageLoader('http://test.positive.com/unknown/page', tempDir)).rejects.toThrow('Request failed with status code 404');
     await expect(fs.access(path.join(tempDir, 'test-positive-com-unknown-page.html'))).rejects
-      .toThrow("ENOENT: no such file or directory, access '/tmp/pageLoader/test-positive-com-unknown-page.html'");
+      .toThrow(/ENOENT: no such file or directory, access '\/tmp\/page-loader-.*\/test-positive-com-unknown-page.html'$/);
   });
 
   test('page should not be saved, if there are no a content', async () => {
     await expect(pageLoader('http://test.positive.com/nocontent/page', tempDir)).rejects.toThrow('Page was not save, status code is 204');
-    await expect(fs.access(path.join(tempDir, 'test-positive-com-unknown-page.html'))).rejects
-      .toThrow("ENOENT: no such file or directory, access '/tmp/pageLoader/test-positive-com-unknown-page.html'");
+    await expect(fs.access(path.join(tempDir, 'test-positive-com-nocontent-page.html'))).rejects
+      .toThrow(/ENOENT: no such file or directory, access '\/tmp\/page-loader-.*\/test-positive-com-nocontent-page.html'$/);
   });
 });
