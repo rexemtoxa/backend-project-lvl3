63ea6a038ae48c3a02ae56cfe816b8d9df7ef9cc
Rehemae Anton <anton.rehemae@enlabs.se>
2020-04-16 01:02:42 +0300

    fix tests

--- __tests__/pageLoader.test.js
+++ __tests__/pageLoader.test.js
@@ -2,14 +2,11 @@ import nock from 'nock';
 import { promises as fs } from 'fs';
 import os from 'os';
 import path from 'path';
-import getFolderSizeCB from 'get-folder-size';
-import util from 'util';
 import debug from 'debug';
 import pageLoader from '../src';
 
 const logger = debug('nock');
 
-const getFolderSize = util.promisify(getFolderSizeCB);
 const genNameTempDir = () => path.join(os.tmpdir(), 'page-loader-');
 const getPathToFixture = (pathToFile) => path.join(__dirname, '__fixtures__', ...pathToFile);
 let tempDir;
@@ -56,46 +53,6 @@ describe('get http request to page and save html', () => {
   });
 });
 
-describe('amount of file should be equal amount of local resources', () => {
-  // Arrange
-  beforeEach(async () => {
-    tempDir = await fs.mkdtemp(genNameTempDir());
-  });
-
-  beforeAll(async () => {
-    nock('http://check.amount.local.files.com')
-      .get('/without/local/files')
-      .replyWithFile(200, getPathToFixture(['pageWithOutLocalResources.html']))
-      .get('/with/local/files')
-      .replyWithFile(200, getPathToFixture(['pageWithLocalResources', 'pageWithLocalResources.html']))
-      .get('/assets/download.ico')
-      .replyWithFile(200, getPathToFixture(['pageWithLocalResources', 'assets', 'download.ico']))
-      .get('/assets/logo.jpg')
-      .replyWithFile(200, getPathToFixture(['pageWithLocalResources', 'assets', 'logo.jpg']))
-      .get('/assets/index.js')
-      .replyWithFile(200, getPathToFixture(['pageWithLocalResources', 'assets', 'index.js']))
-      .get('/assets/style.css')
-      .replyWithFile(200, getPathToFixture(['pageWithLocalResources', 'assets', 'style.css']))
-      .get('/assets/error')
-      .replyWithError({ message: 'went smth wrong', status: 500 })
-      .log(logger);
-  });
-
-  test('page without local resources should be save without assets folders', async () => {
-    // Act
-    await pageLoader('http://check.amount.local.files.com/without/local/files', tempDir);
-    // Assert
-    await expect(fs.readdir(tempDir)).resolves.toHaveLength(1);
-  });
-
-  test('amount of files should depends on references to local resources', async () => {
-    // Act
-    await pageLoader('http://check.amount.local.files.com/with/local/files', tempDir);
-    // Assert
-    await expect(fs.readdir(`${tempDir}/check-amount-local-files-com-with-local-files_files`)).resolves.toHaveLength(4);
-  });
-});
-
 describe('load page with local resources and change the src', () => {
   // Arrange
   beforeEach(async () => {
@@ -107,8 +64,6 @@ describe('load page with local resources and change the src', () => {
     nock('http://check.amount.local.files.com')
       .get('/with/local/files')
       .replyWithFile(200, getPathToFixture(['pageWithLocalResources', 'pageWithLocalResources.html']))
-      .get('/assets/download.ico')
-      .replyWithFile(200, getPathToFixture(['pageWithLocalResources', 'assets', 'download.ico']))
       .get('/assets/logo.jpg')
       .replyWithFile(200, getPathToFixture(['pageWithLocalResources', 'assets', 'logo.jpg']))
       .get('/assets/index.js')
@@ -119,14 +74,21 @@ describe('load page with local resources and change the src', () => {
       .replyWithError({ message: 'went smth wrong', status: 500 })
       .log(logger);
   });
+  const getOutputPath = (fileName) => path.join(
+    tempDir, 'check-amount-local-files-com-with-local-files_files',
+    fileName,
+  );
   test('size of page with assets should equal the size after loading', async () => {
-    // Arrange
-    const expectedFolderSize = await getFolderSize(getPathToFixture(['pageWithLocalResources', 'assets']));
     // Act
     await pageLoader('http://check.amount.local.files.com/with/local/files', tempDir);
-    const actualFolderSize = await getFolderSize(path.join(tempDir, 'check-amount-local-files-com-with-local-files_files'));
+    const expectImage = await fs.readFile(getPathToFixture(['pageWithLocalResources', 'assets', 'logo.jpg']));
+    const actualScript = await fs.readFile(getOutputPath('index.js'), 'utf-8');
+    const actualStyle = await fs.readFile(getOutputPath('style.css'), 'utf-8');
+    const actualImage = await fs.readFile(getOutputPath('logo.jpg'));
     // Assert
-    expect(actualFolderSize).toEqual(expectedFolderSize);
+    expect(actualScript).toMatchSnapshot();
+    expect(actualStyle).toMatchSnapshot();
+    expect(actualImage).toEqual(expectImage);
   });
 });
 
